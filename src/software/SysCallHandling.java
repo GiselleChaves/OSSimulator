26d03b2f98c1379120676af92b7ef5b820782754
Giselle Chaves <112208391+GiselleChaves@users.noreply.github.com>
2025-08-24 17:14:41 -0300

    Initial Commit

---
+++ src/software/SysCallHandling.java
@@ -0,0 +1,31 @@
+package software;
+
+import hardware.HW;
+
+// ------- C H A M A D A S D E S I S T E M A - rotinas de tratamento
+public class SysCallHandling {
+    private HW hw; // referencia ao hw se tiver que setar algo
+
+    public SysCallHandling(HW _hw) {
+        hw = _hw;
+    }
+
+    public void stop() { // chamada de sistema indicando final de programa
+        // nesta versao cpu simplesmente pára
+        System.out.println("SYSCALL STOP");
+    }
+
+    public void handle() { // chamada de sistema
+        // suporta somente IO, com parametros
+        // reg[8] = in ou out    e reg[9] endereco do inteiro
+        System.out.println("SYSCALL para:  " + hw.cpu.getReg()[8] + " / " + hw.cpu.getReg()[9]);
+
+        if  (hw.cpu.getReg()[8]==1){
+            // leitura ...
+
+        } else if (hw.cpu.getReg()[8]==2){
+            // escrita - escreve o conteuodo da memoria na posicao dada em reg[9]
+            System.out.println("OUT:   "+ hw.mem.pos[hw.cpu.getReg()[9]].p);
+        } else {System.out.println("  PARAMETRO INVALIDO"); }
+    }
+}
